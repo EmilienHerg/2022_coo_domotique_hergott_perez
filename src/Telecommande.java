41387edb9949ec27529eac5d0f6e6c1da8545c54
perez112u <perez.ioann5@etu.univ-lorraine.fr>
2022-04-26 14:48:11 +0200

    fvfv

---
+++ src/Telecommande.java
@@ -0,0 +1,32 @@
+public class Telecommande {
+
+
+    public Telecommande() {
+        throw new Error (" code non ecrit ");
+    }
+
+    public void ajouterLampe(Lampe l) {
+        throw new Error (" code non ecrit ");
+    }
+
+    public void activerLampe(int indiceLampe) {
+        throw new Error (" code non ecrit ");
+    }
+
+    public void desactiverLampe(int indiceLampe) {
+        throw new Error (" code non ecrit ");
+    }
+
+    public void activerTout() {
+        throw new Error (" code non ecrit ");
+    }
+
+    public String toString() {
+        throw new Error (" code non ecrit ");
+    }
+
+    public Lampe getLampe(int inciceLampe){
+        throw new Error (" code non ecrit ");
+    }
+
+}
