56ac1808da5b6d178bf8a23a75e03b739b44f530
perez112u <perez.ioann5@etu.univ-lorraine.fr>
2022-04-29 09:27:40 +0200

    ajoute cheminee

--- src/Appareil.java
+++ src/Appareil.java
@@ -1,5 +1,4 @@
 public interface Appareil {
-
     /**
      * permet d allumer un appareil
      */
---
+++ src/ChemineeAdaptee.java
@@ -0,0 +1,18 @@
+public class ChemineeAdaptee implements Appareil {
+
+    private Cheminee ch;
+
+    public ChemineeAdaptee (Cheminee c) {
+        this.ch = c;
+    }
+
+    public void allumer() {
+        int lumiere = this.ch.getLumiere() + 10;
+        this.ch.changerIntensite(lumiere);
+    }
+
+    public void eteindre() {
+        this.ch.changerIntensite(0);
+    }
+
+}
